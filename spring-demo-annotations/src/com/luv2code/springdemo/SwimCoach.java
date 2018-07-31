faaa75505a2f8323b61b4e1d47cc7a9db04419f1
Md Nasim <GS-1618@GSLAB.COM>
2018-07-31 21:33:15 +0530

    Spring config using Java Files.

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -5,20 +5,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SwimCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
 	@Value("${foo.email}")
 	private String email;
 	    
 	@Value("${foo.team}")
 	private String team;
 	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
-		return email;
+		return "Swim 1000 mater as a warm up.";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		return team;
+		return fortuneService.getFortune();
 	}
 
 }
