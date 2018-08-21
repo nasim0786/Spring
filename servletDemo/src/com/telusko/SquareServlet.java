d61559adaa104dbca840d36140464e07cd89b33f
Md Nasim <GS-1618@GSLAB.COM>
2018-08-21 20:45:39 +0530

    ServletContext, ServletConfig Annotation WebServlet.

--- servletDemo/src/com/telusko/SquareServlet.java
+++ servletDemo/src/com/telusko/SquareServlet.java
@@ -3,12 +3,14 @@ package com.telusko;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/sq")
 public class SquareServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
@@ -22,8 +24,6 @@ public class SquareServlet extends HttpServlet{
 			if (cookie.getName().equals("k"))
 				k = Integer.parseInt(cookie.getValue());
 		}
-		
-		
 		
 		k = k * k;
 		PrintWriter  out = res.getWriter();
