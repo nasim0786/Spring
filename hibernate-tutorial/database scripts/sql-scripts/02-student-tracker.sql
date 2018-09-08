d730bbf1c246f64708818f7caf2701757c2882cd
Md Nasim <GS-1618@GSLAB.COM>
2018-09-08 17:16:30 +0530

    Added DB scripts.

--- hibernate-tutorial/database scripts/sql-scripts/02-student-tracker.sql
+++ hibernate-tutorial/database scripts/sql-scripts/02-student-tracker.sql
@@ -11,10 +11,19 @@ CREATE TABLE `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
-  `dob` DATETIMEDEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-ALTER TABLE `hb_student_tracker`.`student`
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `dob` DATETIME DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+ALTER TABLE `hb_student_tracker`.`student` 
 ADD COLUMN `dob` DATETIME NULL AFTER `last_name`;
